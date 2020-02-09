9f1ab8df34f78cfa3c5a5274c99121ed1e6ee833
suraj220487 <49473141+suraj220487@users.noreply.github.com>
2020-02-09 11:20:08 +0530

    Sample Rest service

---
+++ src/test/java/com/example/clarify/clarify/ClarifyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.clarify.clarify;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClarifyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
