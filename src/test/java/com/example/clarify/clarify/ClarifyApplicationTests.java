a9750856b6f599bbcc1abdf2ffcd27da54aa1a8e
Suraj Kumar <49473141+suraj220487@users.noreply.github.com>
2023-11-06 17:27:21 +0530

    comment jnuit

--- src/test/java/com/example/clarify/clarify/ClarifyApplicationTests.java
+++ src/test/java/com/example/clarify/clarify/ClarifyApplicationTests.java
@@ -3,9 +3,9 @@ package com.example.clarify.clarify;
 import java.net.URI;
 import java.net.URISyntaxException;
 
-import org.junit.Assert;
-import org.junit.jupiter.api.Test;
-import org.junit.runner.RunWith;
+// import org.junit.Assert;
+// import org.junit.jupiter.api.Test;
+// import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.boot.test.web.client.TestRestTemplate;
